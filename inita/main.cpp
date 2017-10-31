53cdbb1634297c0cc9bb145a46ccdf57e0aafbff
Jakub Trmal <trmaljak@fel.cvut.cz>
2017-10-31 15:17:23 +0100

    complete init dirs

---
+++ inita/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Ahoj trmaljak" << endl;
+    int number;
+    cin >> number;
+    cout << "your number was " << number << endl;
+    return 0;
+}
