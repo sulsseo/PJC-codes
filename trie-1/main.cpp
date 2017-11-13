2112515bb920ffa88f8300c72694b2887448bbc4
Jakub Trmal <trmaljak@fel.cvut.cz>
2017-11-13 01:10:26 +0100

    cleaning unnecessary files - initb, small1, trie-1

--- trie-1/main.cpp
+++ trie-1/main.cpp
@@ -1,20 +1,6 @@
-//
-// Created by Jakub Trmal on 24.10.17.
-//
-#include <string>
 #include <iostream>
-#include "trie.hpp"
-
-using namespace std;
-
-void init(trie &trie) {
-    trie.root = new trie_node;
-}
 
 int main() {
-    trie_node *struct_field[10];
-    struct_field[1] = new trie_node;
-    struct_field[1]->payload = 'a';
-    struct_field[1]->is_terminal = true;
-    cout << struct_field[1]->payload << endl;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
